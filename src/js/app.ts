a237b26cf6a50be68f42180a3ae8e883dc286d83
Francis Lagares <francis.lagares@gmail.com>
2022-01-08 20:21:53 +0100

    feat: smooth scrolling navigation

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,18 +1,23 @@
 /// ////////////////////////////////////
-// Modal window
-
+// DOM Elements
+/// ////////////////////////////////////
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-const openModal = function (e: Event) {
+/// ////////////////////////////////////
+// Modal window
+/// ////////////////////////////////////
+const openModal = (e: Event) => {
   e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
@@ -25,5 +30,29 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', e => {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+/// ////////////////////////////////////
+// Button Scrolling
+/// ////////////////////////////////////
+btnScrollTo.addEventListener('click', () => {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+/// ////////////////////////////////////
+// Page Navigation
+/// ////////////////////////////////////
+document.querySelector('.nav__links').addEventListener('click', (e: Event) => {
+  e.preventDefault();
+
+  // You need to explicitly tell TypeScript
+  // the type of the HTMLElement which is your target.
+  const target = e.target as HTMLLinkElement;
+
+  if (target.classList.contains('nav__link')) {
+    const id = target.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
