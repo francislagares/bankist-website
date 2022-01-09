cee1094eca7871667e8f873d0132b082bca07a70
Francis Lagares <francis.lagares@gmail.com>
2022-01-09 13:48:32 +0100

    feat: implement tabs component

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,16 +1,11 @@
 /// ////////////////////////////////////
-// DOM Elements
+// Modal window
 /// ////////////////////////////////////
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
-/// ////////////////////////////////////
-// Modal window
-/// ////////////////////////////////////
 const openModal = (e: Event) => {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -36,6 +31,9 @@ document.addEventListener('keydown', e => {
 /// ////////////////////////////////////
 // Button Scrolling
 /// ////////////////////////////////////
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
 btnScrollTo.addEventListener('click', () => {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
@@ -55,4 +53,35 @@ document.querySelector('.nav__links').addEventListener('click', (e: Event) => {
 
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
+});
+
+/// ////////////////////////////////////
+// Tabbed Content
+/// ////////////////////////////////////
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', (e: Event) => {
+  const target = e.target as HTMLButtonElement;
+
+  const clicked: HTMLDivElement = target.closest('.operations__tab');
+
+  // Guard Clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+
+  tabsContent.forEach(tabContent =>
+    tabContent.classList.remove('operations__content--active'),
+  );
+
+  // Activate tabs
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
