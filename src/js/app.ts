435b00f1a775503353b6b4871254e6d3ac31e220
Francis Lagares <francis.lagares@gmail.com>
2022-01-09 18:49:51 +0100

    feat: reveal sections on scroll

--- src/js/app.ts
+++ src/js/app.ts
@@ -118,11 +118,9 @@ nav.addEventListener('mouseout', handleHover.bind('1'));
 // Sticky Navigation - Intersection Observer API
 /// ///////////////////////////////////////////////
 const header = document.querySelector('.header');
-const navHeight = header.getBoundingClientRect().height;
 
 const stickyNav = (entries: IntersectionObserverEntry[]) => {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
@@ -131,6 +129,33 @@ const stickyNav = (entries: IntersectionObserverEntry[]) => {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null as Element,
   threshold: 0,
-  rootMargin: `-${navHeight}px`,
 });
+
 headerObserver.observe(header);
+
+/// //////////////////////////////////////
+// Reveal Sections on scroll
+/// //////////////////////////////////////
+const allSections = document.querySelectorAll('.section');
+
+const sectionReveal = (
+  entries: IntersectionObserverEntry[],
+  observer: IntersectionObserver,
+) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(sectionReveal, {
+  root: null as Element,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
